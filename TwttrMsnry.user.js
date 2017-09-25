86fa46732196f1cf66126d09bd39e27cad83b867
micahcraig <micah@micahcraig.net>
2017-09-25 19:53:05 -0400

    Update TwttrMsnry.user.js

--- TwttrMsnry.user.js
+++ TwttrMsnry.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Twitter Masonry
+// @name         TwttrMsnry
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Apply as Masonry layout to Twitter.com.
