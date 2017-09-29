f076b1a9a74f9a14bf5627bb76f7f815745666b7
micahcraig <micah@micahcraig.net>
2017-09-28 22:13:14 -0400

    Update TwttrMsnry.user.js
    * throttle event calls
    * save state
    * better style targeting
    * simpler (no) masonry options

--- TwttrMsnry.user.js
+++ TwttrMsnry.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         TwttrMsnry
+// @name         Twttr Msnry
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Apply as Monry layout to Twitter.com.
+// @version      0.4
+// @description  Apply a Masonry layout to Twitter.com.
 // @author       Micah Craig (@micahcraig)
 // @match        https://twitter.com/
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @require      https://unpkg.com/masonry-layout@4.2.0/dist/masonry.pkgd.js
 // ==/UserScript==
@@ -18,7 +19,7 @@
     addGlobalStyle(
         '#page-container { width: 100% !important; } ' +
         '.dashboard { width: 15%; } ' +
-        '.content-main .stream>.stream-items:not(.recap-module) .stream-item { float: left; width: 32%; border-radius: 0.25em; margin: 0.25em; } ' +
+        '.content-main .stream>.stream-items:not(.recap-module)>.stream-item { float: left; width: 32%; border-radius: 0.25em; margin: 0.25em; } ' +
         '.dashboard-right { position: absolute; top: 60em; } ' +
         '.content-main { width: 83%; } ' +
         '.content-main-wide { width: 98%; } ' +
@@ -34,20 +35,22 @@
         '.Icon--subtract:before { content: \"\\2012\" } ' );
     $('.stream-container').append("<div class='twt-msnry-ctrl'><a id='increase-columns' class='Icon Icon--medium Icon--add'></a><a id='toggle-dash' class='Icon Icon--medium Icon--dotsVertical'></a><a id='decrease-columns' class='Icon Icon--medium Icon--subtract '></a></div>");
 
-    var numColumns = 3;
-    var $grid = $('.stream>.stream-items:not(.conversation-module):not(.recap-module)').masonry({
-        itemSelector: '.stream-item'
-    });
+    var COLUMNS_VALUE   = "COLUMNS_VALUE",
+        DEFAULT_COLUMNS = 3,
+        DASH_VALUE      = "DASH_VALUE",
+        numColumns      = GM_getValue(COLUMNS_VALUE, DEFAULT_COLUMNS),
+        dashVisible     = GM_getValue(DASH_VALUE, true),
+        $grid = $('.stream>.stream-items:not(.conversation-module):not(.recap-module)').masonry({});
 
-    $grid.on("DOMNodeInserted",function() {
+    if(! dashVisible) { toggleDash(); }
+    if(numColumns != DEFAULT_COLUMNS) { updateColumns(numColumns); }
+
+    $grid.on("DOMNodeInserted", throttle(function() {
+        console.log(arguments);
         $grid.masonry('reloadItems').masonry('layout');
-    });
+    }, 1000));
 
-    $('#toggle-dash').click(function() {
-        $('.dashboard').toggle();
-        $('.content-main').toggleClass('content-main-wide');
-        $grid.masonry('layout');
-    });
+    $('#toggle-dash').click(function() { toggleDash(! dashVisible); });
     $('#increase-columns').click(function() {
         updateColumns(numColumns + 1);
     });
@@ -55,11 +58,33 @@
         updateColumns(numColumns - 1);
     });
 
+    function toggleDash(newVal) {
+        dashVisible = newVal;
+        $('.dashboard').toggle();
+        $('.content-main').toggleClass('content-main-wide');
+        $grid.masonry('layout');
+        //dashVisible = (! dashVisible);
+        GM_setValue(DASH_VALUE, dashVisible);
+    }
+
     function updateColumns(num) {
         numColumns = Math.max(0, num);
         var wdth = Math.floor(100/numColumns) - 0.75;
-        addGlobalStyle('.content-main .stream>.stream-items:not(.recap-module) .stream-item { width:' + wdth + '%; }' );
+        addGlobalStyle('.content-main .stream>.stream-items:not(.recap-module)>.stream-item { width:' + wdth + '%; }' );
         $grid.masonry('layout');
+        GM_setValue(COLUMNS_VALUE, numColumns);
+    }
+
+    // Thanks https://remysharp.com/2010/07/21/throttling-function-calls
+    function throttle(fn, delay) {
+        var timer = null;
+        return function () {
+            var context = this, args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                fn.apply(context, args);
+            }, delay);
+        };
     }
 
     function addGlobalStyle(css) {
